a11eedac49801af65efeaf96e8b2a1d667ece637
Miroslav Mlynarik <mmlynarik@sk.uss.com>
2023-09-18 10:42:15 +0200

    Finalize datamodule

---
+++ src/trams/datamodule.py
@@ -0,0 +1,88 @@
+import logging
+from pathlib import Path
+from typing import Optional
+
+from datasets import load_from_disk, DatasetDict
+from lightning import LightningDataModule
+from lightning.pytorch.utilities.types import EVAL_DATALOADERS, TRAIN_DATALOADERS
+from torch.utils.data import DataLoader
+
+from trams.dataset import load_dataset_from_wav_files, process_dataset, split_dataset
+from trams.config import RAW_DATA_DIR_TRAIN, ARROW_DATA_DIR
+
+log = logging.getLogger(__name__)
+log.addHandler(logging.NullHandler())
+
+
+class TramsDataModule(LightningDataModule):
+    def __init__(
+        self,
+        batch_size: int,
+        validation_split: float,
+        raw_data_dir: Path = RAW_DATA_DIR_TRAIN,
+        arrow_data_dir: Path = ARROW_DATA_DIR,
+    ):
+        super().__init__()
+        self.batch_size = batch_size
+        self.validation_split = validation_split
+        self.raw_data_dir = raw_data_dir
+        self.arrow_data_dir = arrow_data_dir
+
+    def prepare_data(self) -> DatasetDict:
+        if Path(self.arrow_data_dir / "dataset_dict.json").exists():
+            log.info(f"Processed dataset already exists in {self.arrow_data_dir} folder.")
+            return load_from_disk(self.arrow_data_dir)
+
+        if not self.raw_data_dir.exists():
+            log.error(f"Raw wav files were not found. Please supply them in {self.raw_data_dir}")
+            return
+
+        dataset = load_dataset_from_wav_files()
+        dataset = split_dataset(dataset, validation_pct=self.validation_split)
+        dataset = process_dataset(dataset)
+        return dataset
+
+    def setup(self, stage: Optional[str] = None) -> None:
+        dataset = load_from_disk(self.arrow_data_dir)
+        dataset = dataset.remove_columns(
+            ["sample_rate", "num_frames", "num_channels", "bits_per_sample", "path", "label_name", "audio"]
+        )
+
+        if stage is None or stage == "fit":
+            self.train_dataset = dataset["train"]
+        if stage is None or stage in ["fit", "validate"]:
+            self.val_dataset = dataset["validation"]
+        if stage is None or stage == "test":
+            pass
+
+    def train_dataloader(self) -> TRAIN_DATALOADERS:
+        return DataLoader(
+            self.train_dataset,
+            batch_size=self.batch_size,
+            shuffle=True,
+            num_workers=4,
+            pin_memory=True,
+        )
+
+    def val_dataloader(self) -> EVAL_DATALOADERS:
+        return DataLoader(
+            self.val_dataset,
+            batch_size=self.batch_size,
+            shuffle=False,
+            num_workers=4,
+            pin_memory=True,
+        )
+
+    def test_dataloader(self) -> EVAL_DATALOADERS:
+        pass
+
+
+if __name__ == "__main__":
+    dm = TramsDataModule(batch_size=16, validation_split=0.1)
+    dm.prepare_data()
+    dm.setup()
+
+    for step, batch in enumerate(dm.val_dataloader()):
+        if step == 0:
+            print(batch)
+            break
--- src/trams/dataset.py
+++ src/trams/dataset.py
@@ -1,21 +1,60 @@
 import os
 import jsonlines
 from pathlib import Path
-from typing import Any
+from typing import Any, Callable
+import random
 
 import torchaudio
 import pandas as pd
 from torchaudio.backend.common import AudioMetaData
-from datasets import load_dataset, load_from_disk, Dataset, DatasetDict
+from torchaudio import transforms
+from datasets import load_dataset, Dataset, DatasetDict
 from IPython.display import display
 
-from trams.config import RAW_DATA_DIR_TRAIN, ARROW_DATA_DIR
+from trams.config import RAW_DATA_DIR_TRAIN, ARROW_DATA_DIR, MAX_LENGTH_SECS, NUM_FFT, NUM_MELS, MAX_DB
 
 
-def load_dataset_from_wav_files(validation_pct: float, cached: bool = True):
-    if ARROW_DATA_DIR.exists() and any(ARROW_DATA_DIR.iterdir()) and cached:
-        return load_from_disk(ARROW_DATA_DIR)
+Batch = dict[str, Any]
 
+
+def _add_label_name(batch: Batch, int2str: Callable) -> Batch:
+    return {"label_name": [int2str(label) for label in batch["label"]]}
+
+
+def _get_max_frames(dataset: Dataset) -> int:
+    sample_rate = dataset["train"]["sample_rate"][0]
+    return int(MAX_LENGTH_SECS * sample_rate)
+
+
+def _flatten_example_dict(batch: Batch) -> Batch:
+    return {
+        "audio": [(items["array"]) for items in batch["audio"]],
+        "path": [items["path"] for items in batch["audio"]],
+    }
+
+
+def _truncate(batch: Batch, max_frames: int) -> Batch:
+    return {"audio": [audio[:max_frames] for audio in batch["audio"]]}
+
+
+def _pad_one_audio(audio: list[float], max_frames: int) -> list[float]:
+    padding_size = max_frames - len(audio)
+    padding_left = random.randint(0, padding_size)
+    padding_right = padding_size - padding_left
+    return [0] * padding_left + audio + [0] * padding_right
+
+
+def _pad(batch: Batch, max_frames: int) -> Batch:
+    return {"audio": [_pad_one_audio(audio, max_frames) for audio in batch["audio"]]}
+
+
+def _get_mel_spectrogram(
+    batch: Batch, mel_spectrogram: transforms.MelSpectrogram, amplitude_transformer: transforms.AmplitudeToDB
+) -> Batch:
+    return {"spectrogram": [amplitude_transformer(mel_spectrogram(audio)) for audio in batch["audio"]]}
+
+
+def load_dataset_from_wav_files() -> DatasetDict:
     with jsonlines.open(RAW_DATA_DIR_TRAIN / "metadata.jsonl", mode="w") as writer:
         for idx, (root, _, files) in enumerate(os.walk(RAW_DATA_DIR_TRAIN)):
             if idx == 0:
@@ -34,25 +73,36 @@ def load_dataset_from_wav_files(validation_pct: float, cached: bool = True):
                 writer.write(metadata)
 
     dataset = load_dataset("audiofolder", data_dir=RAW_DATA_DIR_TRAIN, drop_labels=False)
-    dataset = dataset["train"].train_test_split(validation_pct, stratify_by_column="label", seed=100)
-    dataset = DatasetDict({"train": dataset["train"], "validation": dataset["test"]})
-    dataset.save_to_disk(ARROW_DATA_DIR)
     return dataset
 
 
-def process_dataset(dataset: Dataset):
-    def add_label_name(batch: dict[str, Any]):
-        train_dataset: Dataset = dataset["train"]
-        return {"label_name": [train_dataset.features["label"].int2str(label) for label in batch["label"]]}
+def split_dataset(dataset: DatasetDict, validation_pct: float) -> DatasetDict:
+    dataset = dataset["train"].train_test_split(validation_pct, stratify_by_column="label", seed=100)
+    return DatasetDict({"train": dataset["train"], "validation": dataset["test"]})
 
-    def flatten_example_dict(batch: dict[str, Any]):
-        return {
-            "audio": [(items["array"]) for items in batch["audio"]],
-            "path": [items["path"] for items in batch["audio"]],
-        }
 
-    dataset = dataset.map(add_label_name, batched=True)
-    dataset = dataset.map(flatten_example_dict, batched=True, remove_columns=["audio"])
+def process_dataset(dataset: DatasetDict) -> DatasetDict:
+    train_dataset: Dataset = dataset["train"]
+    int2str = train_dataset.features["label"].int2str
+    max_frames = _get_max_frames(dataset)
+    sample_rate = train_dataset["sample_rate"][0]
+    mel_spectrogram = transforms.MelSpectrogram(sample_rate, n_fft=NUM_FFT, n_mels=NUM_MELS)
+    amplitude_transformer = transforms.AmplitudeToDB(top_db=MAX_DB)
+
+    dataset = (
+        dataset.map(_add_label_name, batched=True, fn_kwargs={"int2str": int2str}, desc="Add label name")
+        .map(_flatten_example_dict, batched=True, remove_columns=["audio"], desc="Flatten example dict")
+        .map(_truncate, batched=True, fn_kwargs={"max_frames": max_frames}, desc="Truncate")
+        .map(_pad, batched=True, fn_kwargs={"max_frames": max_frames}, desc="Pad")
+        .with_format("torch", columns=["audio", "label"])
+        .map(
+            _get_mel_spectrogram,
+            batched=True,
+            fn_kwargs={"mel_spectrogram": mel_spectrogram, "amplitude_transformer": amplitude_transformer},
+            desc="Generate Mel Spectrogram",
+        )
+    )
+    dataset.save_to_disk(ARROW_DATA_DIR)
     return dataset
 
 
