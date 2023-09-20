8a0c1a2f7c9b150074b4d158995772a356fa7f92
Miroslav Mlynarik <mmlynarik@sk.uss.com>
2023-09-20 17:58:48 +0200

    Add predict script draft

--- src/trams/config.py
+++ src/trams/config.py
@@ -8,6 +8,9 @@ RAW_DATA_DIR_TEST = RAW_DATA_DIR / "test"
 ARROW_DATA_DIR = DATA_DIR / "arrow"
 
 MAX_LENGTH_SECS = 4
+ONE_TENTH_SEC = 0.1
 NUM_FFT = 1024
 NUM_MELS = 64
 MAX_DB = 90
+
+TRAINDED_MODEL_PATH = ROOT_DIR / "src" / "trams" / "trained_model" / "model.ckpt"
